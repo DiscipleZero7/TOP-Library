96aa922b6ff7703b69b62d7cf3fe1b5b8753880a
Kyle Hines <disciplezero7@gmail.com>
2025-12-30 11:44:49 -0600

    Allow adding of new books

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const myLibrary = [];
+const libraryContainer = document.querySelector(".libraryContainer");
+const addBookBtn = document.querySelector(".add-book-btn");
+
+const bookForm = document.querySelector(".book-form");
+
+const modal = document.querySelector(".modal-form");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+const submitBookBtn = document.querySelector(".submit-book-btn");
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -15,10 +23,43 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
+
+    //add new book to library
+
+    const bookCard = document.createElement("div")
+
+    bookCard.textContent = `${book.title}, ${book.author}, ${book.pages} pages - Status: ${book.read}`;
+
+    libraryContainer.appendChild(bookCard);
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "Have read");
 addBookToLibrary("Animal Farm", "George Orwell", 84, "Have read");
 addBookToLibrary("Treasure Island", "Robert Louise Stevenson", 271, "Haven't read");
 
-console.log(myLibrary);
+addBookBtn.addEventListener("click", () => {
+    modal.showModal();
+})
+
+modalCloseBtn.addEventListener("click", () => {
+    modal.close();
+})
+
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const bookTitle = document.querySelector("#title");
+    const bookAuthor = document.querySelector("#author");
+    const bookPages = document.querySelector("#pages");
+    let bookReadStatus = document.querySelector("#read-status");
+
+    if (bookReadStatus.checked === true) {
+        bookReadStatus = "Have read";
+    } else {
+        bookReadStatus = "Haven't read";
+    }
+
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookReadStatus)
+
+    console.log(myLibrary)
+})
